95125bfac8c7c7ae1be72771469e96d4f3ea1b9e
unknown <dev.hamidshahbazi@gmail.com>
2022-11-13 16:27:04 +0330

    1401.08.22 after install navigation but error in apk

--- App/App.js
+++ App/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import axios from 'axios';
+
 import Register from './Auth/Register/Register.js';
 import Login from './Auth/Login/Login';
 import SplashScreen from './SplashScreen/SplashScreen';
-import axios from 'axios';
+
 // axios.defaults.baseURL = 'https://ksp-shop.com/';
 axios.defaults.baseURL = 'http://192.168.100.15:8000/api/';
+
+
+const Tab = createBottomTabNavigator();
+
 export default App = () => {
     return (
-        <React.Fragment>
-            {/*<Login/>*/}
-            <Register/>
-            {/*<SplashScreen/>*/}
-        </React.Fragment>
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Register" component={Register} />
+                <Tab.Screen name="Login" component={Login} />
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 };
 
